8c62a6787d533a73b5000e22b4cab5a639383254
Sapir <sapir.rahmani@gmail.com>
2019-01-01 11:47:14 +0200

    new project

--- app/src/main/java/com/example/sapir/todoapplication/NewTaskFragment.kt
+++ app/src/main/java/com/example/sapir/todoapplication/NewTaskFragment.kt
@@ -17,7 +17,7 @@ class NewTaskFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_add_new_task, container, false)
+        return inflater.inflate(R.layout.fragment_add_new_task, container, false)
     }
 
 }
