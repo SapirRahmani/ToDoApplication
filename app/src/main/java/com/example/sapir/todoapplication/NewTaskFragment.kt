05e87eb0698804c68156d252a4c6755d0d3ebf10
Sapir <sapir.rahmani@gmail.com>
2019-01-01 11:30:59 +0200

    new project

---
+++ app/src/main/java/com/example/sapir/todoapplication/NewTaskFragment.kt
@@ -0,0 +1,23 @@
+package com.example.sapir.todoapplication
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class NewTaskFragment : Fragment() {
+
+
+    companion object {
+
+        fun newInstance(): NewTaskFragment {
+            return NewTaskFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_add_new_task, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/sapir/todoapplication/ToDoTask.kt
@@ -0,0 +1,8 @@
+package com.example.sapir.todoapplication
+
+import java.util.*
+
+data class ToDoTask(var title: String?, var description: String, val createDate: Date = Date()) {
+
+
+}
